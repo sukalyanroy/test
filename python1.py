a68e7bb334d65ff4571c03af31b909dcd14111a3
Sukalyan <sukalyanroy3@gmail.com>
2015-09-18 12:22:53 +0100

    This is the first commit

---
+++ python1.py
@@ -0,0 +1,13 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Fri Sep 11 09:31:03 2015
+
+@author: suman
+"""
+
+import math
+
+r = input("Enter the radius of the sphere")
+
+volume = 4.0/3.0 * math.pi * (r*r*r)
+print ("Volume of the sphere is:" + str(volume))
